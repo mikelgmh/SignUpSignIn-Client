cac1a40daf71f483a815e88652f46212dbadc9d7
Mikel <mikelgmh@gmail.com>
2020-10-20 12:01:25 +0200

    Creación de clases necesarias para el proyecto

--- src/signable/SignableFactory.java
+++ src/signable/SignableFactory.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package signupsignin.client;
+package signable;
 
 /**
  *
  * @author Mikel
  */
-public class SignUpSignInClient {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class SignableFactory {
     
 }
